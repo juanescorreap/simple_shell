186cdda9a806cbc5d8870180e6ddb02e0e4f8458
juanescorreap <juanestebancope@outlook.com>
2022-10-27 18:30:29 -0500

    prints a prompt, waits  * for a commnad and reads it form the stream

--- _prompt.c
+++ _prompt.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 /**
  * _prompt - Function that prints a prompt, waits
- * for a commnad and reads it form the stream.
+ * for a commnad and reads it form the stream
  * @buffer: String that store characters typed
  * @buffersize: The size of the buffer
  * Return: The caracters readed, or -1 if fails
